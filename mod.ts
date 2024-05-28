59dfe6c243cbb339f064eddaea00e6de9280f9de
Xkonti <xkonti@xkonti.tech>
2024-05-28 02:08:59 -0400

    Export additional types

--- mod.ts
+++ mod.ts
@@ -1,8 +1,23 @@
+import type { ApiInfo } from "./api-info.ts";
+import type {
+    EndpointDefBase,
+    EndpointDefinition,
+    InEndpointDefinition,
+    OutEndpointDefinition,
+    InOutEndpointDefinition
+} from "./endpoint-definitions.ts";
 import type { HttpVerb } from "./http-verb.ts";
-import type { InEndpointDefinition, OutEndpointDefinition, InOutEndpointDefinition } from "./endpoint-definitions.ts";
+
 import { GptApi } from "./gpt-api.ts";
 
+export type { ApiInfo };
+export type { HttpVerb };
+export type {
+    EndpointDefBase,
+    EndpointDefinition,
+    InEndpointDefinition,
+    OutEndpointDefinition,
+    InOutEndpointDefinition
+};
 
-export type { HttpVerb }
-export type { InEndpointDefinition, OutEndpointDefinition, InOutEndpointDefinition };
 export { GptApi };
