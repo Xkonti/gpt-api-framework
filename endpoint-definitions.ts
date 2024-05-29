066478d06588b81dc79b497b79b8ca00fdfceb9b
Xkonti <xkonti@xkonti.tech>
2024-05-29 01:37:54 -0400

    Add missing docs

--- endpoint-definitions.ts
+++ endpoint-definitions.ts
@@ -1,6 +1,10 @@
 import type {HttpVerb} from "./http-verb.ts";
 import type { z } from "zod";
 
+/**
+ * Base interface for defining an endpoint.
+ * This does not include request or response schemas.
+ */
 export interface EndpointDefBase {
     verb: HttpVerb;
     path: string;
@@ -8,6 +12,10 @@ export interface EndpointDefBase {
     operationId: string;
 }
 
+/**
+ * Used o define an endpoint with request and response schemas
+ * along with descriptions for each.
+ */
 export interface EndpointDefinition extends EndpointDefBase {
     requestSchema: z.Schema | null;
     requestDesc: string | null;
@@ -15,14 +23,26 @@ export interface EndpointDefinition extends EndpointDefBase {
     responseDesc: string | null;
 }
 
+/**
+ * Used to define an input-only endpoint with a request schema,
+ * but no response schema.
+ */
 export interface InEndpointDefinition<TRequestSchema extends z.Schema> extends EndpointDefBase {
     requestSchema: TRequestSchema;
 }
 
+/**
+ * Used to define an output-only endpoint with a response schema,
+ * but no request schema.
+ */
 export interface OutEndpointDefinition<TResponseSchema extends z.Schema> extends EndpointDefBase {
     responseSchema: TResponseSchema;
 }
 
+/**
+ * Used to define an input/output endpoint with both a request and
+ * response schema.
+ */
 export interface InOutEndpointDefinition<TRequestSchema extends z.Schema, TResponseSchema extends z.Schema>
     extends EndpointDefBase {
     requestSchema: TRequestSchema;
--- schema-builder.ts
+++ schema-builder.ts
@@ -4,6 +4,11 @@ import { zodToJsonSchema } from "zod-to-json-schema";
 import type { EndpointDefinition } from "./endpoint-definitions.ts";
 import type { HttpVerb } from "./mod.ts";
 
+/**
+ * Returns the JSON schema for a Zod schema.
+ * @param schema The Zod schema to convert.
+ * @returns The generated JSON schema.
+ */
 export function getSchemaDesc(schema: z.Schema | null){
     if (!schema) return null;
     return zodToJsonSchema(schema, {
@@ -12,6 +17,9 @@ export function getSchemaDesc(schema: z.Schema | null){
     }).definitions?.schema ?? null;
 }
 
+/**
+ * Describes the schema of an endpoint.
+ */
 export interface EndpointSchema {
     description: string;
     operationId: string;
@@ -37,13 +45,21 @@ export interface EndpointSchema {
 
 }
 
+/**
+ * Describes the paths of an OpenAPI spec.
+ */
 export type Paths = {
     [key: string]: Partial<{
         [key in HttpVerb]: EndpointSchema
     }>
 };
 
-export function getPathsDesc(endpoints: EndpointDefinition[]) {
+/**
+ * Assembles the paths of an OpenAPI spec from endpoint definitions.
+ * @param endpoints The endpoint definitions to use.
+ * @returns The paths of the OpenAPI spec.
+ */
+export function getPathsDesc(endpoints: EndpointDefinition[]): Paths {
     const paths: Paths = {};
     for (const endpoint of endpoints) {
         // This is to allow multiple endpoints with the same path but different verbs
@@ -80,6 +96,15 @@ export function getPathsDesc(endpoints: EndpointDefinition[]) {
     return paths;
 }
 
+/**
+ * Generates an OpenAPI spec for the defined API.
+ * @param url The URL of the API.
+ * @param title The title of the API.
+ * @param description The description of the API.
+ * @param version The version of the API.
+ * @param endpoints The endpoint definitions to use.
+ * @returns The OpenAPI spec.
+ */
 export function getOpenApiSpec(
     url: string,
     title: string,
